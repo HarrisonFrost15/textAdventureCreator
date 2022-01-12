4cd45b90a45b7bb6d869b1e899c79354cdca3963
RobLR <robert.leroux10@gmail.com>
2022-01-12 00:10:34 +0000

    Added a gameWorld object
    All players, items and places are stored in a single object
    This allows each thing to be referenced a lot more easily
    Unforunately broke the structure of the main script

--- classes.ts
+++ classes.ts
@@ -1,61 +1,43 @@
 "use strict"
 
+// Game world class that will be used to store the player and all locations, so that it can be saved and provide easy reference to each location.
+class World{
+    worldName: string
+    player: Player
+    places: Record <string, Place> = {}
+    items: Record <number, Item> = {}
+    startText: string
+
+    constructor(worldName:string, player: Player, startText: string){
+        this.worldName = worldName
+        this.player = player
+        this.startText = startText
+    }
+
+    addPlace(name:string, place:Place){
+        this.places[name]=place
+    }
+
+    addItem(id:number, item:Item){
+        this.items[id]=item
+    }
+}
+
 // Sets the player class, including all of the properties related to the player, for example where they are
 class Player{ 
     inventory: Record <string, Item>
     place: Place
-    time: number
+    time: number = 0
     alive: boolean = true
     health: number = 20
     stamina: number = 10
 
-    constructor(place:Place, time: number, alive : boolean, health : number, stamina : number){
+    constructor(place: Place, health: number, stamina: number){
         this.inventory ={}
         this.place = place
-        this.time = time
-        this.alive = alive 
         this.health = health
         this.stamina = stamina
     }
-}
-
-// Defines items that exist in the world, these can be obstacles or items the player can pick up.
-class Item {
-    // itemName: string
-    // weight: number
-    // place: Place
-    description: string = "No further information"
-    examination:string;
-    alight: boolean = false
-    broken: boolean = false
-    locked:boolean = false
-    collectable:boolean
-    open:boolean = false
-    hidden:boolean = true
-    contents: Record <string, Item> = {}
-    pushable: boolean = false
-    edible : boolean = false
-    drinkable : boolean = false
-    poisonous: boolean = false
-    breakable: boolean 
-    throwable: boolean
-    flammable: boolean
-
-    // Constructor for new items.
-    constructor(public itemName:string, public weight:number, public place:Place, description:string, examination:string, collectable:boolean, breakable:boolean, throwable:boolean, flammable:boolean) { 
-        this.itemName = itemName
-        this.weight = weight
-        this.place = place
-        this.description = description
-        this.examination = examination
-        this.collectable = collectable
-        this.breakable = breakable
-        this.throwable = throwable
-        this.flammable = flammable
-    }
-
-    // Methods related to items
-    
 }
 
 // Creates a class for places including properties such as including other nearby places, what items are in this location
@@ -84,8 +66,8 @@ class Place{
         `
     }
 
-//     // Links a new place to the current one and also creates a reverse link so you can go back to the previous place
-    addPlace(direction:string, place:Place, exit:Exit){
+    // Links a new place to the current one and also creates a reverse link so you can go back to the previous place
+    addNearbyPlace(direction:string, place:Place, exit:Exit){
         this.nearby[direction]=place
         this.exits[direction]=exit
         let previousDirection = ""
@@ -109,7 +91,6 @@ class Place{
         }
         place.nearby[previousDirection] = this
         place.exits[previousDirection] = exit
-        return place // return a reference to the place we just added (so we can chain adds)
     }
 
     addItem(name:string, item:Item){
@@ -124,3 +105,43 @@ class Exit {
         this.locked = locked
     }
 }
+
+// Defines items that exist in the world, these can be obstacles or items the player can pick up.
+class Item {
+    itemID: number
+    itemName: string
+    description: string
+    weight: number
+    // parentContainer: object
+    contents: Record <string, Item> = {}
+    alight: boolean = false
+    broken: boolean = false
+    locked:boolean = false
+    collectable:boolean = false
+    open:boolean = false
+    hidden:boolean = true
+    pushable: boolean = false
+    edible : boolean = false
+    drinkable : boolean = false
+    poisonous: boolean = false
+    breakable: boolean = true
+    throwable: boolean = true
+    flammable: boolean = true
+
+    // Constructor for new items.
+    constructor(itemID:number, itemName:string, weight:number, /*parentContainer:object,*/ description:string) { 
+        this.itemID = itemID
+        this.description = description
+        this.itemName = itemName
+        this.weight = weight
+        // this.parentContainer = parentContainer
+    }
+
+    // Methods related to items
+    
+    addItem(name:string, item:Item){
+        this.contents[name]=item
+    }
+
+}
+
